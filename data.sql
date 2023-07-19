12b7c15c02911d0479b0e9e397d697d388952aa9
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-20 05:51:01 +0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,7 @@
 
 INSERT INTO animals VALUES ('0', 'Agumon', '2020-02-03', '0', 'true', '10.23');
 INSERT INTO animals VALUES ('1', 'Gabumon', '2018-11-15', '2', 'true', '8');
-INSERT INTO animals VALUES ('2', 'Biyomon', '2021-01-07', '1', 'false', '15.04');
-INSERT INTO animals VALUES ('3', 'Tentomon', '2017-05-012', '5', 'true', '11');
+INSERT INTO animals VALUES ('2', 'Devimon', '2017-05-12', '5', 'true', '11');
+INSERT INTO animals VALUES ('3', 'Pikachu', '2021-01-07', '1', 'false', '15.04');
+
 
