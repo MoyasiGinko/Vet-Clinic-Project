db5f57311f928f7cdaba14f66a55e91177ae0240
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-21 10:31:43 +0600

    Add fix changes

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name varchar(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
