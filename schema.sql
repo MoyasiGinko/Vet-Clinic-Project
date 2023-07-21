ee69b7bc4cd049550373a5560155716321476bd6
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-21 16:47:03 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE animals (
     weight_kg DECIMAL(5,2) NOT NULL
 );
 
-/*Add a column species of type string to your animals table.*/
+/*Add a column species of type string to animals table.*/
 ALTER TABLE animals ADD COLUMN species varchar(100);
 
 /*Create a table named owners*/
@@ -26,7 +26,7 @@ CREATE TABLE species (
     name varchar(100) NOT NULL
 );
 
-/*Add a column species_id of type integer to your animals table.*/
+/*Add a column species_id and owner_id to animals table.*/
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
