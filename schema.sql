7b1e84809395bb47298f56832536410436a86572
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-21 16:30:42 +0600

    Create species and owners table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,22 @@ CREATE TABLE animals (
 /*Add a column species of type string to your animals table.*/
 ALTER TABLE animals ADD COLUMN species varchar(100);
 
+/*Create a table named owners*/
+CREATE TABLE owners (
+    id SERIAL NOT NULL PRIMARY KEY,
+    full_name varchar(100) NOT NULL,
+    age INT NOT NULL
+);
+
+
+/*Create a table named species*/
+CREATE TABLE species (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+/*Add a column species_id of type integer to your animals table.*/
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
